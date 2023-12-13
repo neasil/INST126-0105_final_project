1b5da6f774958fb0ef9618c06d6cb8c29ed61916
sarina li <67284676+neasil@users.noreply.github.com>
2023-12-13 00:09:01 -0500

    Utilizing dictionaries and creating visualizations
    Changed how the collected counts are stored. Rather than using a simple array, I opted to using a dictionary. This allowed me to create a data frame which could then be visualized into either a scatter plot, bar plot, or line plot. I allowed the user to choose what type of plot they wanted.

--- final_project.py
+++ final_project.py
@@ -2,12 +2,14 @@
 from bs4 import BeautifulSoup
 import requests
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Prompting user to link to Wikipedia page
 wiki_link = input("Please input a link to a Wikipedia page:\t").strip()
 
 # Prompting user for words to count in the Wikipedia page
-words = input("Please input words to count. You may enter as many you'd like. They should be separated by commas.\t").lower().strip()
+words = input("Please input words to count. You may enter as many you'd like. They should be separated by commas.\t").lower().replace(".", ",").strip()
 
 # Converting inputted words into a list 
 words_to_count = words.split(",")
@@ -17,7 +19,6 @@ fixed_words_count = np.array([])
 for word in np.arange(len(words_to_count)):
     fixed_word = words_to_count[word].strip()
     fixed_words_count = np.append(fixed_words_count, fixed_word)
-fixed_words_count
 
 # Extracting URL
 wiki_page = requests.get(wiki_link)
@@ -31,18 +32,32 @@ wiki_text = wiki_soup.get_text().lower()
 # Making website text into a readable list
 text_countable = wiki_text.split()
 
-# Store counts of inputted words into an array
-data = np.array([])
+# Storing data in a dictionary 
+counts = {"Word": [], "Count": []}
+
+# Store counts of inputted words into the dictionary
 for w in np.arange(len(fixed_words_count)):
     print(fixed_words_count[w])
-    counts = text_countable.count(fixed_words_count[w])
-    data = np.append(data, counts)
+    count = text_countable.count(fixed_words_count[w])
+    print(count)
+    counts["Word"].append(fixed_words_count[w])
+    counts["Count"].append(count)
 
-# Printing results to user
-data
+# Converting the data in a data frame that can be plotted 
+counts_table = pd.DataFrame.from_dict(counts)
 
+# Asking the user how they would like to visualize their data
+visualization = input("How would you like to visualize your data? Please enter either either 'scatterplot', 'barplot', or 'lineplot':\t").lower().strip()
 
-
-
+# Visualizing the data based on whatever the user chooses
+if visualization == "scatterplot":
+    plt.scatter("Word", "Count", data = counts_table)
+    plt.show()
+elif visualization == "barplot":
+    plt.bar("Word", "Count", data = counts_table)
+    plt.show()
+elif visualization == "lineplot":
+    plt.plot("Word", "Count", data = counts_table)
+    plt.show()
 
 
